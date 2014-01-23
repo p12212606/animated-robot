bb06b8dad585464f8575668ff2f30951f20c86a3
Bhavik Patel <P12212606@GH652-015.lec-admin.dmu.ac.uk>
2014-01-23 14:50:11 +0000

    Updated with remainder function

---
+++ maths_branching/maths_functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int add (int x, int y)
+{
+    return x + y;
+}
+
+int subtract (int x, int y)
+{
+    return x - y;
+}
+
+int multiply (int x, int y)
+{
+    return x * y;
+}
+
+int divide (int x, int y)
+{
+   return x / y;
+}
+
+int remainder (int x, int y)
+{
+	return x % y;
+}
+
+int main() {
+	
+	
+
+}
