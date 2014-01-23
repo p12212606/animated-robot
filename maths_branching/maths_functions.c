da8eb0a7e573f86483ae2b7bf079b3aca5f523c0
Bhavik Patel <P12212606@GH652-015.lec-admin.dmu.ac.uk>
2014-01-23 14:51:54 +0000

    updated with comment

---
+++ maths_branching/maths_functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int add (int x, int y)
+{
+    return x + y;
+}
+
+int subtract (int x, int y)
+{
+    return x - y;
+}
+
+int multiply (int x, int y)
+{
+    return x * y;
+}
+
+int divide (int x, int y)
+{
+   return x / y;
+}
+
+int main() {
+	
+	//test
+
+}
